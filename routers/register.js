8347a84ffabddfc4dffaabc48109fd23446fd65c
YbiG-blog <yash2010146@akgec.ac.in>
2022-08-02 01:44:48 +0530

    accout update and delete add

--- routers/register.js
+++ routers/register.js
@@ -3,15 +3,38 @@ const router = new express.Router();
 const Patient = require("../schemas/patient");
 const Hospital = require("../schemas/hospital");
 
-router.get("/registerpatient", async (req, res) => {
+router.get("/patient/:id", async (req, res) => {
   try {
-    const data = await Patient.find();
-    res.status(201).send(data);
+    const id = req.params.id;
+    const data = await Patient.findById(id);
+    res.status(200).send(data);
   } catch (err) {
     res.status(400).send(err);
   }
 });
-
+// update patient
+router.patch("/patient/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Patient.findOneAndUpdate(id,{
+      $set: req.body
+    });
+      res.status(200).send("Account Updated");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+// delete patient
+router.delete("/patient/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Patient.findByIdAndDelete(id);
+    res.status(200).json("Account deleted");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+});
 router.post("/registerpatient", async (req, res) => {
   const otp = Math.floor(Math.floor(100000 + Math.random() * 900000));
   try {
@@ -41,15 +64,38 @@ router.post("/registerpatient", async (req, res) => {
   }
 });
 
-router.get("/registerhospital", async (req, res) => {
+router.get("/hospital/:id", async (req, res) => {
   try {
-    const data = await Hospital.find();
-    res.status(201).send(data);
+    const id = req.params.id;
+    const data = await Hospital.findById(id);
+    res.status(200).send(data);
   } catch (err) {
     res.status(400).send(err);
   }
 });
-
+// update hospital
+router.patch("/hospital/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Hospital.findOneAndUpdate(id,{
+      $set: req.body
+    });
+      res.status(200).send("Account Updated");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+// delete hospital
+router.delete("/hospital/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Hospital.findByIdAndDelete(id);
+    res.status(200).json("Account deleted");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+});
 router.post("/registerhospital", async (req, res) => {
   // const otp = Math.floor(Math.floor(100000 + Math.random() * 900000));
   try {
