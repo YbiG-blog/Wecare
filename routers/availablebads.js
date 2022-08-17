e1964a4cd83fb2942b80a3be9780893bb19d7d8b
YbiG-blog <yash2010146@akgec.ac.in>
2022-08-17 22:17:51 +0530

    bdas check by any way

--- routers/availablebads.js
+++ routers/availablebads.js
@@ -3,7 +3,7 @@ const Hospital = require("../schemas/hospital");
 const Bads = require("../schemas/bads")
 const router = new express.Router();
 
-router.get("/total/bads", async (req, res) => {
+router.get("/totalnum/bads", async (req, res) => {
     try {
       const availableBads = await Bads.find({
         booking : false,
@@ -16,6 +16,30 @@ router.get("/total/bads", async (req, res) => {
       res.sendStatus(500);
       return;
     }
-  });
+});
+router.get("/bads/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+      const findBad = await Bads.findById(id); 
+    //   console.log( findBad );
+      res.status(200).send(findBad);
+      return;
+    } catch (err) {
+    res.status(400).send(`err ${err}`);
+      return;
+    }
+});
+
+router.get("/bads", async (req, res) => {
+    try {
+      const findBads = await Bads.find(); 
+    //   console.log( findBads );
+      res.status(200).send(findBads);
+      return;
+    } catch (err) {
+    res.status(400).send(`err ${err}`);
+      return;
+    }
+});
 
   module.exports = router;
