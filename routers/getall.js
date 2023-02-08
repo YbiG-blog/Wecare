0e709455a848934ec4387bd0e613a6f731faa3c8
YbiG-blog <yash2010146@akgec.ac.in>
2023-02-08 17:34:51 +0530

    reduced load time in district list

--- routers/getall.js
+++ routers/getall.js
@@ -15,7 +15,7 @@ router.get("/hospitals", async (req, res) => {
 router.get("/hospitals/:city", async ({ params }, res) => {
   try {
     const { city } = params;
-    const allHospital = await Hospital.find( { city: city }, { password: 0 });
+    const allHospital = await Hospital.find({ city: city }, { password: 0 });
     if (allHospital.length === 0) {
       return res.status(404).send("No bed available in this area");
     }
@@ -27,7 +27,7 @@ router.get("/hospitals/:city", async ({ params }, res) => {
 router.get("/hospitalsbypin/:pincode", async ({ params }, res) => {
   try {
     const { pincode } = params;
-    const allHospital = await Hospital.find( { pincode: pincode }, { password: 0 }  );
+    const allHospital = await Hospital.find({ pincode: pincode }, { password: 0 });
     if (allHospital.length === 0) {
       return res.status(404).send("No bed available in this area");
     }
@@ -38,23 +38,19 @@ router.get("/hospitalsbypin/:pincode", async ({ params }, res) => {
 });
 router.get("/data-district", async (req, res) => {
   try {
-   const findCity = await Hospital.aggregate([
-    {$unwind: "$city"}, {$group :  { _id :"$city"} }, {$project : { _id : 1 }},
-   ]);
-   let dataArray  = [];
-   for (const e in findCity) {
-    let val = await Bed.aggregate([
-      {$match :{ city : findCity[e]._id }},
-      { $group: {
-        bedCount : { $sum : { $add: ["$generalType.availbility","$specialType.availbility"]}},
-        hospitalsCount : { $sum : 1 } ,
-        _id:0 },} ]);
-  dataArray.push({"city" : findCity[e]._id, "availableBeds" : val})
-   };
-   return res.status(200).json(dataArray); 
+    const dataArray = await Bed.aggregate([
+      { $unwind: "$city" },
+      {
+        $group: {
+          _id: "$city",
+          bedCount: { $sum: { $add: ["$generalType.availbility", "$specialType.availbility"] } },
+          hospitalsCount: { $sum: 1 }
+        }
+      }]);
+    return res.status(200).json(dataArray);
   } catch (e) {
     console.log(`error : ${e.message}`);
-   return res.status(500).send(`err ${e}`);
+    return res.status(500).send(`err ${e}`);
   }
 });
 
