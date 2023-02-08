8e95a9ae9606fb79fa68e274c6214c868ad345b7
YbiG-blog <yash2010146@akgec.ac.in>
2023-02-08 19:07:46 +0530

    update in login/signup folder

--- routers/login.js
+++ routers/login.js
@@ -5,7 +5,7 @@ const router = new express.Router();
 const jwt = require("jsonwebtoken");
 
 // login route for hospital
-router.post("/loginhospital", async ({ body }, res) => {
+router.post("/login", async ({ body }, res) => {
   try {
     const { password, email } = body;
 
@@ -15,24 +15,17 @@ router.post("/loginhospital", async ({ body }, res) => {
       // token generate
       const pay_load = { _id: hospitalCheck._id };
       const cookie_token = jwt.sign(pay_load, process.env.TOKEN_SECRET_KEY);
-
-      //add cookie
-      res.cookie("jwt_shi23", cookie_token, {
-        secure: true,
-        expires: new Date(Date.now() + 864000000),
-        httpOnly: false,
-      });
+      // //add cookie
+      // res.cookie("jwt_shi23", cookie_token, { //   secure: true,
+      //   expires: new Date(Date.now() + 864000000),  //   httpOnly: false,    // });
       if (match_password) {
-        res.status(200).send({
-          message: " hospital logged in successfully",
-          cookie_token: cookie_token,
-          hosId: hospitalCheck._id,
-        });
+       return res.status(200).send({
+          message: " hospital logged in successfully", cookie_token: cookie_token, hosId: hospitalCheck._id  });
       } else {
-        res.status(400).send({ msg: "Wrong Password" });
+       return res.status(401).send({ msg: "Wrong Password" });
       }
     } else {
-      res.status(400).send({ msg: "Invalid details" });
+     return res.status(500).send({ msg: "Invalid details" });
     }
   } catch (err) {
     console.log(err);
--- routers/register.js
+++ routers/register.js
@@ -7,115 +7,80 @@ const saltRounds = 10;
 const jwt = require("jsonwebtoken");
 
 // get  post req
-router.post("/hospitalbyid", async ({ body }, res) => {
+router.post("/hospitalbyId", async ({ body }, res) => {
   try {
-    const Id = body.Id;
-    const dataHos = await Hospital.findById(Id);
-    const bedData = await Beds.findOne(
-      { hospitalId: Id },
-      {
-        // projection
-        _id: 0,
-        generalType: 1,
-        specialType: 1,
-        otherFacilities:1,
-      }
-    );
-    console.log(bedData.generalType.availbility);
-    res.status(200).send({ dataHos, bedData });
+    const { _id } = body;
+    const dataArray = await Beds.findOne({ hospitalId: _id }).populate({ path:"hospitalId" });
+    if(!dataArray) return res.status(401).json( "This id does not have any account" );
+   return res.status(200).send( dataArray );
   } catch (err) {
-    res.status(400).send(err);
+    console.log(`err : ${ err.message }`);    
+    return res.status(500).json(err);
   }
 });
+
 router.get("/hospital/:_id", async ({ params }, res) => {
   try {
     const { _id } = params;
-    const dataHos = await Hospital.findById(_id);
-    const bedData = await Beds.findOne(
-      { hospitalId: Id },
-      {
-        // projection
-        _id: 0,
-        generalType: 1,
-        specialType: 1,
-      }
-    );
-    res.status(200).send({ dataHos, bedData });
+    const dataArray = await Beds.findOne({ hospitalId: _id }).populate({ path:"hospitalId" });
+    if(!dataArray) return res.status(401).json( "This id does not have any account" );
+   return res.status(200).send( dataArray );
   } catch (err) {
-    res.status(400).send(err);
+    console.log(`err : ${ err.message }`);    
+    return res.status(500).json(err);
   }
 });
+
 // update hospital
-router.patch("/hospital/:id", async ({ body, params }, res) => {
+router.patch("/hospital/:_id", async ({ body, params }, res) => {
   try {
-    const Id = params.id;
-   const t = await Hospital.findOneAndUpdate(
-      {
-        _id: Id,
-      },
-      {
-        $set: body,
-      }
-    );
-    res.status(200).send("Account Updated");
+    const { _id } = params;
+   const data = await Hospital.findByIdAndUpdate( _id,
+      { $set: body } );
+    if(!data) return res.status(401).json("This id dose not have any account.");
+    return res.status(200).send({ msg :"Account Updated", data : data });
   } catch (err) {
-    res.status(400).send(err);
+    console.log(`err : ${ err.message }`);    
+   return res.status(500).json(err);
   }
 });
 // delete hospital
-router.delete("/hospital/:id", async ({ params }, res) => {
+router.delete("/hospital/:_id", async ({ params }, res) => {
   try {
-    const Id = params.id;
-    const data = await Hospital.findByIdAndDelete(Id);
-    res.status(200).json("Account deleted");
+    const { _id } = params;
+    const data = await Hospital.findByIdAndDelete( _id );
+    if(!data) return res.status(401).json("This id dose not have any account.");
+    return res.status(200).json("Account deleted");
   } catch (err) {
-    console.log(err);
-    return res.status(400).json(err);
+    console.log(`err : ${ err.message }`);
+   return  res.status(500).json(err);
   }
 });
 
-router.post("/registerhospital", async (req, res) => {
+router.post("/register", async ({ body }, res) => {
   try {
-    const {
-      name,
-      email,
-      mobileNum,
-      password,
-      address,
-      state,
-      city,
-      pincode,
-      hospitalType,
-    } = await req.body;
+    let {
+      name, email, mobileNum, password, address, state, city, pincode, hospitalType,
+    } =  body;
     const hospitalExist = await Hospital.findOne({ email });
-
     if (hospitalExist) {
-      return res.status(200).send({ msg: "already exists." });
+      return res.status(403).send({ msg: "already exists." });
     }
     // const cookie_token = await  Hospital.generateAuthToken();
+     city = city.toLowerCase();
     const hospital_create = new Hospital({
-      name,
-      email,
-      mobileNum,
-      password,
-      address,
-      state,
-      city,
-      pincode,
-      hospitalType,
-    });
+    name, email, mobileNum, password, address, state, city, pincode, hospitalType  });
     // password encryption------------
-    hospital_create.password = await bcrypt.hash(
-      hospital_create.password,
-      saltRounds
-    );
+    hospital_create.password = await bcrypt.hash( hospital_create.password,  saltRounds  );
     const save = await hospital_create.save();
+
     // token
     const pay_load = { _id: hospital_create._id };
-    const token = jwt.sign(pay_load, process.env.TOKEN_SECRET_KEY);
-    res.status(201).send({ save, token, });
+    const token = jwt.sign( pay_load, process.env.TOKEN_SECRET_KEY);
+
+    return res.status(201).send({ save, token, });
   } catch (err) {
-    res.status(400).send(`error ${err}`);
+   return res.status(400).send(`error ${err}`);
   }
 });
 
